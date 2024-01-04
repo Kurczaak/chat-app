2cdca2a9d9f8ece4e59db3e9d87b01499d96a8fc
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-04 12:19:37 +0100

    Refactor API endpoints and add global prefix

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; // Import the ConfigModule from the appropriate package
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'; // Import the TypeOrmModule from the appropriate package
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_URL, 
+      autoLoadEntities: true,
+      synchronize: true,
+
+  
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
