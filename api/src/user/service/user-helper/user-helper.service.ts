f81e32a7fd230bb9406cdf89db74bb5dec207c05
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-14 17:33:42 +0100

    Add user helper methods for DTO conversion

--- api/src/user/service/user-helper/user-helper.service.ts
+++ api/src/user/service/user-helper/user-helper.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { Observable, of } from 'rxjs';
+import { CreateUserDto } from 'src/user/model/dto/create-user.dto';
+import { LoginUserDto } from 'src/user/model/dto/login-user.dto';
+import { UserI } from 'src/user/model/user.interface';
 
 @Injectable()
-export class UserHelperService {}
+export class UserHelperService {
+    createUserDtoToUserEntity(createUserDto: CreateUserDto): Observable<UserI>{
+        return of({
+            username: createUserDto.username,
+            email: createUserDto.email,
+            password: createUserDto.password
+        })
+    }
+
+    loginUserDtoToEntity(loginUserDto: LoginUserDto): Observable<UserI>{
+    return of({
+        email: loginUserDto.email,
+        password: loginUserDto.password,
+    })
+    }
+
+}
