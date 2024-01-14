dafa08fc6f8c90a2a4aaa3e6d6828d530599c335
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-14 15:47:16 +0100

    Refactor user controller to include create, findAll, and login endpoints

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { UserService } from '../service/user-service/user.service';
+import { Observable, of } from 'rxjs';
+import { UserI } from '../model/user.interface';
+import { CreateUserDto } from '../model/dto/create-user.dto';
+import { LoginUserDto } from '../model/dto/login-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    constructor(
+        private userService: UserService
+    ){}
+
+    @Post()
+    create(@Body() createUserDto: CreateUserDto ): Observable<boolean> {
+        return of(true);
+    }
+
+    @Get()
+    findAll(){
+
+    }
+
+    @Post()
+    login(@Body() user: LoginUserDto): Observable<boolean>{
+        return of(true);
+    }
+}
