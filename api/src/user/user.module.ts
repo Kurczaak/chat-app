d61cc4c79f79449c1da01fee5bf98c603eeb38b4
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-14 13:11:56 +0100

    feat: add initial user service

--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
+import { UserController } from './controller/user.controller';
+import { UserService } from './service/user.service';
 
 @Module({
-  controllers: [UserController]
+  controllers: [UserController],
+  providers: [UserService]
 })
 export class UserModule {}
