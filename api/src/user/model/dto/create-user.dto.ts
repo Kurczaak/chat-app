0bc211b9287aed901daaf25dfb06ab6ceec4f2c7
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-14 15:46:58 +0100

    Add global validation pipe and create user DTO

---
+++ api/src/user/model/dto/create-user.dto.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty, IsString } from "class-validator";
+import { LoginUserDto } from "./login-user.dto";
+
+
+export class CreateUserDto extends LoginUserDto{
+
+    @IsString()
+    @IsNotEmpty()
+    username: string;
+}
