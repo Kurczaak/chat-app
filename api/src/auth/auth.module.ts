f5c549858224a7c48dd0734a99d8b0c77845a803
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-15 17:05:57 +0100

    change directories for auth modules

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './controller/auth.controller';
+import { AuthService } from './service/auth.service';
+
 
 @Module({
   controllers: [AuthController],
