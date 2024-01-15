061e7db2fbc8f34a8d04e6d3acf10abc80d0ec99
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-15 17:26:33 +0100

    Add JWT module and configure secret

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controller/auth.controller';
 import { AuthService } from './service/auth.service';
-
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '10000s',
+        },
+      }),
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class AuthModule {}
