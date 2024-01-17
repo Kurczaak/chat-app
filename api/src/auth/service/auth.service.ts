eca4f015811c422119221ca69c7b44da1ca98d71
Michał Kurczak <kurczakmichal97@gmail.com>
2024-01-17 07:11:52 +0100

    Refactor auth.service.ts

--- api/src/auth/service/auth.service.ts
+++ api/src/auth/service/auth.service.ts
@@ -1,17 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Observable, from } from 'rxjs';
+import { UserI } from 'src/user/model/user.interface';
 var bcrypt = require('bcryptjs');
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly jwtService: JwtService) {}
+  constructor(private readonly jwtService: JwtService) {}
 
-     comparePasswords(attemptPassword: string, storedPasswordHash: string): Observable<boolean>{
-        return from( Promise.resolve(bcrypt.compareSync(attemptPassword, storedPasswordHash)));
-      }
+  generateJWT(user: UserI): Observable<string> {
+    return from(this.jwtService.signAsync({ user }));
+  }
 
-     hashPassword(password: string): Observable<string>{
-        return from<string>(bcrypt.hash(password, 12));
-    }
+  comparePasswords(
+    attemptPassword: string,
+    storedPasswordHash: string
+  ): Observable<boolean> {
+    return from(
+      Promise.resolve(bcrypt.compareSync(attemptPassword, storedPasswordHash))
+    );
+  }
+
+  hashPassword(password: string): Observable<string> {
+    return from<string>(bcrypt.hash(password, 12));
+  }
 }
