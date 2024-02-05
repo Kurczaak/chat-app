72cb7c9ef2a49ce5a0952378293671da4f05d678
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-05 20:54:05 +0100

    Add initial ChatModule to app.module.ts

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -11,6 +11,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AuthMiddleware } from './middleware/auth.middleware';
+import { ChatModule } from './chat/chat.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
@@ -24,6 +25,7 @@ import { AuthMiddleware } from './middleware/auth.middleware';
     }),
     UserModule,
     AuthModule,
+    ChatModule,
   ],
   controllers: [AppController],
   providers: [AppService],
