976a27b80e475443756922d6e09197b46c17cda0
Michał Kurczak <kurczakmichal97@gmail.com>
2024-02-24 08:59:52 +0100

    chore: change observables to promises in user controller

--- api/src/user/controller/user.controller.ts
+++ api/src/user/controller/user.controller.ts
@@ -1,6 +1,5 @@
 import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { UserService } from '../service/user-service/user.service';
-import { Observable, map, of, switchMap } from 'rxjs';
 import { UserI } from '../model/user.interface';
 import { CreateUserDto } from '../model/dto/create-user.dto';
 import { LoginUserDto } from '../model/dto/login-user.dto';
@@ -17,18 +16,18 @@ export class UserController {
   ) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto): Observable<UserI> {
-    return this.userHelperService
-      .createUserDtoToUserEntity(createUserDto)
-      .pipe(switchMap((user: UserI) => this.userService.create(user)));
+  async create(@Body() createUserDto: CreateUserDto): Promise<UserI> {
+    const userEntity =
+      await this.userHelperService.createUserDtoToUserEntity(createUserDto);
+    return this.userService.create(userEntity);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  findAll(
+  async findAll(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10
-  ): Observable<Pagination<UserI>> {
+  ): Promise<Pagination<UserI>> {
     limit = limit > 100 ? 100 : limit;
     return this.userService.findAll({
       page,
@@ -38,19 +37,13 @@ export class UserController {
   }
 
   @Post('login')
-  login(@Body() user: LoginUserDto): Observable<LoginResponseI> {
-    return this.userHelperService.loginUserDtoToEntity(user).pipe(
-      switchMap((user: UserI) =>
-        this.userService.login(user).pipe(
-          map((jwt: string) => {
-            return {
-              access_token: jwt,
-              token_type: 'JWT',
-              expires_in: 10000,
-            };
-          })
-        )
-      )
-    );
+  async login(@Body() user: LoginUserDto): Promise<LoginResponseI> {
+    const userEntity = await this.userHelperService.loginUserDtoToEntity(user);
+    const jwt = await this.userService.login(userEntity);
+    return {
+      access_token: jwt,
+      token_type: 'JWT',
+      expires_in: 10000,
+    };
   }
 }
