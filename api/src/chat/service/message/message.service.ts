a3a47245163827bd8a0a0cfc592dd5bd70821aa3
Michał Kurczak <kurczakmichal97@gmail.com>
2024-03-09 11:01:59 +0100

    fix: fix getting all messages for all rooms

--- api/src/chat/service/message/message.service.ts
+++ api/src/chat/service/message/message.service.ts
@@ -8,6 +8,7 @@ import {
 import { MessageEntity } from 'src/chat/model/message/message.entity';
 import { MessageI as MessageI } from 'src/chat/model/message/message.interface';
 import { RoomI } from 'src/chat/model/room/room.interface';
+import { FindOptionsUtils } from 'typeorm';
 import { Repository } from 'typeorm/repository/Repository';
 
 @Injectable()
@@ -25,9 +26,14 @@ export class MessageService {
     room: RoomI,
     options: IPaginationOptions
   ): Promise<Pagination<MessageI>> {
-    return paginate(this.messageRepository, options, {
-      room,
-      relations: ['user', 'room'],
-    });
+    const queryBuilder =
+      this.messageRepository.createQueryBuilder('message_entity');
+
+    queryBuilder
+      .where('message_entity.roomId = :roomId', { roomId: room.id })
+      .leftJoinAndSelect('message_entity.user', 'user')
+      .leftJoinAndSelect('message_entity.room', 'room'); // This line is key
+    const msgs = await queryBuilder.getMany();
+    return paginate<MessageI>(queryBuilder, options);
   }
 }
